acf7417dffee9f6e62ddd3954a492ff4016cbc1d
Sahil Jain <145692165+sahilj23@users.noreply.github.com>
2025-10-24 00:06:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+from supporting_functions import (
+    extract_video_id,
+    get_transcript,
+    translate_transcript,
+    generate_notes,
+    get_important_topics,
+    create_chunks,
+    create_vector_store,
+    rag_answer
+)
+
+# --- Sidebar (Inputs) ---
+with st.sidebar:
+    st.title("VidSynth AI")
+    st.markdown("---")
+    st.markdown("Transform any YouTube video into key topics, a podcast, or a chatbot.")
+    st.markdown("### Input Details")
+
+    youtube_url = st.text_input("YouTube URL", placeholder = "https://www.youtube.com/watch?v=...")
+    language = st.text_input("Video Language Code", placeholder = "e.g., en, hi, es, fr", value = "en")
+
+    task_option = st.radio(
+        "Choose what you want to generate:",
+        ["Chat with Video", "Notes For You"]
+    )
+
+    submit_button = st.button("Start Processing")
+    st.markdown("---")
+
+    # The "New Chat" button has been removed from here.
+
+# --- Main Page ---
+st.title("YouTube Content Synthesizer")
+st.markdown("Paste a video link and select a task from the sidebar.")
+
+
+# --Processing Flow--
+if submit_button:
+    if youtube_url and language:
+        video_id = extract_video_id(youtube_url)
+        if video_id:
+            with st.spinner("Step 1/3 : Fetching transcript...."):
+                full_transcript = get_transcript(video_id, language)
+
+                if language != "en":
+                    with st.spinner("Step 1.5/3 : Translating transcript into English, this may take a few moments..."):
+                        full_transcript = translate_transcript(full_transcript)
+
+
+        if task_option == "Notes For You":
+            with st.spinner("Step 2/3 : Extracting important Topics..."):
+                important_topics = get_important_topics(full_transcript)
+                st.subheader("Important Topics")
+                st.write(important_topics)
+                st.markdown("---")
+
+            with st.spinner("Step 3/3 : Generating Notes for you..."):
+                notes = generate_notes(full_transcript)
+                st.subheader("Notes for you")
+                st.write(notes)
+
+            st.success("Summary and Notes Generated")
+
+        if task_option == "Chat with Video":
+            with st.spinner("Step 2/3 : Creating Chunks and Vector store...."):
+                chunks = create_chunks(full_transcript)
+                vectorstore = create_vector_store(chunks)
+                st.session_state.vector_store = vectorstore #created a session named vector_store to store the embeddings
+                # and not recall the function everytime when prompt is called, basically, to give memory, as streamlit in itself
+                # cant store anything
+
+            st.session_state.messages = [] # empty list to store entire chats as ('User' : message by user , 'Assistant' : message by assistant )
+            st.success('Video is ready for chat....')
+
+# chatbot session
+if task_option == "Chat with Video" and "vector_store" in st.session_state:
+    st.divider()
+    st.subheader("Chat with Video")
+
+    # Display the entire history of chats of user and assistant
+    for message in st.session_state.get('messages', []):
+        with st.chat_message(message['role']): # checking sbka role and the display their icon, then their message
+            st.write(message['content'])
+
+    # user input
+    prompt = st.chat_input("Ask me anything about the video.")  # store user's input as prompt
+
+    if prompt:
+        st.session_state.messages.append({'role' : 'user', 'content': prompt})
+        with st.chat_message('user'):   # to create icon for user in front of the text given by user to the chat model
+            st.write(prompt)  # to display the prompt that user wrote
+
+        with st.chat_message('assistant'):
+            response = rag_answer(prompt, st.session_state.vector_store)
+            st.write(response)
+        st.session_state.messages.append({'role': 'assistant', 'content': response})
+
+
---
+++ supporting_functions.py
@@ -0,0 +1,175 @@
+import re
+import streamlit as st
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+import time
+
+load_dotenv()
+
+# Function to extract video ID from a YouTube URL (Helper Function)
+
+def extract_video_id(url):
+    """
+    Extracts the YouTube video ID from any valid YouTube URL.
+    """
+    match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", url)
+    if match:
+        return match.group(1)
+    st.error("Invalid YouTube URL. Please enter a valid video link.")
+    return None
+
+# function to get transcript of the video
+def get_transcript(video_id, language):
+    ytt_api = YouTubeTranscriptApi()
+    try:
+        transcript = ytt_api.fetch(video_id, languages = [language])
+        full_transcript = " ".join([i.text for i in transcript])
+        time.sleep(10)
+        return full_transcript
+    except Exception as e:
+        st.error(f"Error fetching video {e}")
+
+
+# function to translate the transcript to english
+    # initialize the genai model
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash-lite",
+    temperature = 0.2
+)
+
+def translate_transcript(transcript):
+    try:
+        prompt = ChatPromptTemplate.from_template(f"""
+        You are an expert translator with deep cultural and linguistic knowledge.
+        I will provide you with a transcript. Your task is to translate it into English with absolute accuracy, preserving:
+        - Full meaning and context (no omissions, no additions)
+        - Tone and Style (formal/informal, emotional/neutral as in original).
+        - Nuances, idioms, and cultural expressions (adapt appropriately while keeping intent.)
+        - Speaker's voice (same perspective, no rewriting in third-person).
+        Do no summarize or simplify. The translation should read naturally in the target language but stay as close as possible to the original intent.
+        
+        Transcript :
+        {transcript}
+        """)
+
+        # Runnable Chain
+        chain = prompt | llm
+
+        # invoke chain
+        response = chain.invoke({"transcript": transcript})
+
+        return response.content
+
+    except Exception as e:
+        st.error(f"Error fetching transcript {e}")
+
+
+# function to get important topics
+def get_important_topics(transcript):
+    try:
+        prompt = ChatPromptTemplate.from_template(f"""
+                You are an assistant that extracts the 5 most important topics discussed in a video transcript or summary.
+                
+                Rules:
+                - Summarize in exactly 5 major points.
+                - Each point should represent a key topic or concept, not small details.
+                - Keep wording concise and focussed on the technical content.
+                - Do not phrase them as questions or opinions.
+                - Output should be a numbered list.
+                - Show only points that are discussed in the transcript.
+                
+                Here is the transcript:
+                {transcript}    
+                """)
+
+        # Runnable Chain
+        chain = prompt | llm
+
+        # invoke chain
+        response = chain.invoke({"transcript": transcript})
+
+        return response.content
+
+    except Exception as e:
+        st.error(f"Error fetching transcript {e}")
+
+
+# function to get notes from the video
+def generate_notes(transcript):
+    try:
+        prompt = ChatPromptTemplate.from_template(f"""
+                You are an AI note-taker. Your task is to read the following YouTube video transcript and produce well-structured,
+                concise notes.
+                
+                Requirements:
+                - Present the output as **bulleted points**, grouped into clear sections.
+                - Highlight key takeaways, important facts, and examples.
+                - Use **short, clear sentences** (no long paragraphs).
+                - If the transcript includes multiple themes, organize them under **sub-headings**
+                - Do not add information that is not present in the transcript.
+                
+                Here is the transcript:
+                {transcript}
+                """)
+
+        # Runnable Chain
+        chain = prompt | llm
+
+        # invoke chain
+        response = chain.invoke({"transcript": transcript})
+
+        return response.content
+
+    except Exception as e:
+        st.error(f"Error fetching transcript {e}")
+
+
+
+# Function to create chunks
+def create_chunks(transcript):
+    text_splitters = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
+    doc = text_splitters.create_documents([transcript])
+    return doc
+
+# function to creating embeddings of chunks and store it into vector space
+def create_vector_store(docs):
+    embedding = HuggingFaceEmbeddings(model_name = "all-MiniLM-L6-v2")
+    vector_store = Chroma.from_documents(docs, embedding)
+    return vector_store
+
+# RAG function
+def rag_answer(question, vectorstore):
+    results = vectorstore.similarity_search(question, k = 4) # will convert question to embeddings and then retrieve top 4 similar chunks from vector store
+    context_text = "\n".join([i.page_content for i in results]) # combine the 4 chunks' important content (excluding meta data) to create one long paragraph to give it to llm
+
+    prompt = ChatPromptTemplate.from_template("""
+                You are a kind, and precise assistant.
+                - Begin with a warm and respectful greeting (avoid repeating greetings every turn)
+                - Understand the user's intent even with typos or grammatical mistakes.
+                - Answer ONLY using the retrieved context.
+                - If answer not in context, say:
+                  "I couldn't find information in the database. Could you please rephrase or ask something else?"
+                - Keep answers clear, concise, and friendly.
+                
+                Context:
+                {context}
+                
+                Question;  
+                {question}
+                
+                Answer:
+                """)
+
+    # chain
+    chain = prompt | llm
+    response = chain.invoke({"context": context_text, "question":question})
+
+    return response.content
+
+
